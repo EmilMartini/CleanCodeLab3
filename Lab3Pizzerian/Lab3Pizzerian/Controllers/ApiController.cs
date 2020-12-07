50af5f8593f6a2aa735f07bd9d8c14a48d3e46ab
emil.martini <emil.martini@contica.se>
2020-12-07 11:36:01 +0100

    init commit

---
+++ Lab3Pizzerian/Lab3Pizzerian/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Pizzerian.Controllers
+{
+    public class ApiController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateOrder()
+        {
+            return Ok();
+        }
+    }
+}
