f862b677f9c4317afab4ee3f7f963049cc325ed0
emil.martini <emil.martini@contica.se>
2020-12-07 14:53:01 +0100

    added API calls

--- Lab3Pizzerian/Lab3Pizzerian/Controllers/ApiController.cs
+++ Lab3Pizzerian/Lab3Pizzerian/Controllers/ApiController.cs
@@ -6,12 +6,71 @@ using System.Threading.Tasks;
 
 namespace Lab3Pizzerian.Controllers
 {
+    [Route("Api")]
     public class ApiController : Controller
     {
-        [HttpGet]
+        [Route("Order")]
+        [HttpPost]
         public IActionResult CreateOrder()
         {
-            return Ok();
+            MockDb instance = MockDb.GetDbInstance();
+            var order = new Order();
+            order.ID = Guid.NewGuid();
+            var accepted = instance.CreateOrder(order);
+            if (accepted)
+            {
+                return new OkObjectResult($"Order created with OrderId: {order.ID}{Environment.NewLine}" +
+                    $"Save this OrderId to add food and drinks to your order.");
+            } else
+            {
+                return new ConflictObjectResult($"OrderId: {order.ID} already exists.");
+            }
         }
+
+        [Route("Order")]
+        [HttpGet]
+        public IActionResult GetOrder()
+        {
+            MockDb instance = MockDb.GetDbInstance();
+            var orders = instance.GetOrders();
+            if(orders != null)
+            {
+                return new OkObjectResult(orders);
+            } else
+            {
+                return new NoContentResult();
+            }
+        }
+
+        [Route("Order/{OrderId}")]
+        [HttpGet]
+        public IActionResult GetOrder(string orderId)
+        {
+            MockDb instance = MockDb.GetDbInstance();
+            var order = instance.GetOrder(orderId);
+            if (order != null)
+            {
+                return new OkObjectResult(order);
+            }
+            else
+            {
+                return new NoContentResult();
+            }
+        }
+
+        [Route("Menu")]
+        [HttpGet]
+        public IActionResult GetMenu()
+        {
+            MockDb instance = MockDb.GetDbInstance();
+            var pizzas = instance.GetMenu();
+            if(pizzas != null)
+            {
+                return new OkObjectResult(pizzas);
+            } else
+            {
+                return NoContent();
+            }
+        }    
     }
 }
--- Lab3Pizzerian/Lab3Pizzerian/Database/MockDb.cs
+++ Lab3Pizzerian/Lab3Pizzerian/Database/MockDb.cs
@@ -1,6 +1,7 @@
 ﻿using Lab3Pizzerian.Enumerations;
 using Lab3Pizzerian.Extensions;
 using Lab3Pizzerian.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace Lab3Pizzerian
 	public class MockDb
 	{
 		private static MockDb instance = null;
-		public List<Order> Orders { get; set; }
+		public List<Order> Orders { get; set; } = new List<Order>();
 		public List<Pizza> Menu { get; set; }
 		public Dictionary<EnumIngredient, int> Ingredients { get; set; }
 		private readonly Dictionary<EnumDrink, int> Drinks = new Dictionary<EnumDrink, int>
@@ -35,5 +36,33 @@ namespace Lab3Pizzerian
 			return Drinks.Where(x => x.Key == drink).FirstOrDefault().Value;
 		}
 		//samma för ingredients GetIngredientPrice
-	}
+
+		public bool CreateOrder(Order order)
+        {
+            if (Orders.Any(a => a.ID == order.ID))
+            {
+				return false;
+            } else
+            {
+				Orders.Add(order);
+				return true;
+            }
+        }
+
+		public List<Order> GetOrders()
+        {
+			return Orders;
+        }
+
+		public Order GetOrder(string guid)
+		{
+			var order = Orders.Where(i => i.ID.ToString() == guid).FirstOrDefault();
+			return order;
+		}
+
+        public List<Pizza> GetMenu()
+        {
+			return Menu;
+        }
+    }
 }
