3a9f203f16081050963eca91248351606da16f81
emil.martini <emil.martini@contica.se>
2020-12-07 11:48:34 +0100

    add MockDb singleton class

---
+++ Lab3Pizzerian/Lab3Pizzerian/Database/MockDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Pizzerian
+{
+    public class MockDb
+    {
+        private static MockDb instance = null;
+
+        public Dictionary<Ingredient, int> Ingredients { get; set; }
+        public Dictionary<Drink, int> Drinks { get; set; }
+
+        public static MockDb GetDbInstance()
+        {
+            if(instance == null)
+            {
+                instance = new MockDb();
+            }
+            return instance;
+        }
+    }
+}
