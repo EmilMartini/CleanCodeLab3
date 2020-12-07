f75c3791fa45f7e27a66114e695875402acd0e40
Daniel <daniel.andersson@iths.se>
2020-12-07 16:02:36 +0100

    add pizzaMenuModel

--- Lab3Pizzerian/Lab3Pizzerian/Controllers/ApiController.cs
+++ Lab3Pizzerian/Lab3Pizzerian/Controllers/ApiController.cs
@@ -1,4 +1,6 @@
 ﻿using Lab3Pizzerian.Enumerations;
+using Lab3Pizzerian.Extensions;
+using Lab3Pizzerian.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,87 +9,102 @@ using System.Threading.Tasks;
 
 namespace Lab3Pizzerian.Controllers
 {
-    [Route("Api")]
-    public class ApiController : Controller
-    {
-        [Route("Order")]
-        [HttpPost]
-        public IActionResult CreateOrder()
-        {
-            MockDb instance = MockDb.GetDbInstance();
-            var order = new Order();
-            order.ID = Guid.NewGuid();
-            var accepted = instance.CreateOrder(order);
-            if (accepted)
-            {
-                return new OkObjectResult($"Order created with OrderId: {order.ID}{Environment.NewLine}" +
-                    $"Save this OrderId to add food and drinks to your order.");
-            } else
-            {
-                return new ConflictObjectResult($"OrderId: {order.ID} already exists.");
-            }
-        }
+	[Route("Api")]
+	public class ApiController : Controller
+	{
+		[Route("Order")]
+		[HttpPost]
+		public IActionResult CreateOrder()
+		{
+			MockDb instance = MockDb.GetDbInstance();
+			var order = new Order();
+			order.ID = Guid.NewGuid();
+			var accepted = instance.CreateOrder(order);
+			if (accepted)
+			{
+				return new OkObjectResult($"Order created with OrderId: {order.ID}{Environment.NewLine}" +
+					 $"Save this OrderId to add food and drinks to your order.");
+			}
+			else
+			{
+				return new ConflictObjectResult($"OrderId: {order.ID} already exists.");
+			}
+		}
 
-        [Route("Orders")]
-        [HttpGet]
-        public IActionResult GetOrders()
-        {
-            MockDb instance = MockDb.GetDbInstance();
-            var orders = instance.GetOrders();
-            if (orders != null)
-            {
-                return new OkObjectResult(orders);
-            } else
-            {
-                return new NoContentResult();
-            }
-        }
+		[Route("Orders")]
+		[HttpGet]
+		public IActionResult GetOrders()
+		{
+			MockDb instance = MockDb.GetDbInstance();
+			var orders = instance.GetOrders();
+			if (orders != null)
+			{
+				return new OkObjectResult(orders);
+			}
+			else
+			{
+				return new NoContentResult();
+			}
+		}
 
-        [Route("Orders/{OrderId}")]
-        [HttpGet]
-        public IActionResult GetOrder(string orderId)
-        {
-            MockDb instance = MockDb.GetDbInstance();
-            var order = instance.GetOrder(orderId);
-            if (order != null)
-            {
-                return new OkObjectResult(order);
-            }
-            else
-            {
-                return new NoContentResult();
-            }
-        }
+		[Route("Orders/{OrderId}")]
+		[HttpGet]
+		public IActionResult GetOrder(string orderId)
+		{
+			MockDb instance = MockDb.GetDbInstance();
+			var order = instance.GetOrder(orderId);
+			if (order != null)
+			{
+				return new OkObjectResult(order);
+			}
+			else
+			{
+				return new NoContentResult();
+			}
+		}
 
-        [Route("Orders/{OrderStatus:int}")]
-        [HttpGet]
-        public IActionResult GetOrdersByStatus(int OrderStatus)
-        {
-            MockDb instance = MockDb.GetDbInstance();
-            var order = instance.GetOrders((EnumStatus)OrderStatus);
-            if (order != null)
-            {
-                return new OkObjectResult(order);
-            }
-            else
-            {
-                return new NoContentResult();
-            }
-        }
+		[Route("Orders/{OrderStatus:int}")]
+		[HttpGet]
+		public IActionResult GetOrdersByStatus(int OrderStatus)
+		{
+			MockDb instance = MockDb.GetDbInstance();
+			var order = instance.GetOrders((EnumStatus)OrderStatus);
+			if (order != null)
+			{
+				return new OkObjectResult(order);
+			}
+			else
+			{
+				return new NoContentResult();
+			}
+		}
 
-        [Route("Menu")]
-        [HttpGet]
-        public IActionResult GetMenu()
-        {
-            MockDb instance = MockDb.GetDbInstance();
-            var pizzas = instance.GetMenu();
-            if(pizzas != null)
-            {
-                return new OkObjectResult(pizzas);
-            } else
-            {
-                return NoContent();
-            }
-        }    
-    }
+		[Route("Menu")]
+		[HttpGet]
+		public IActionResult GetMenu()
+		{
+			MockDb instance = MockDb.GetDbInstance();
+			var pizzaMenu = instance.GetMenu();
+			var pizzaMenuModel = new List<PizzaMenuModel>();
+			foreach (var pizza in pizzaMenu)
+			{
+				pizzaMenuModel.Add(
+					new PizzaMenuModel
+					{
+						Name = pizza.Name,
+						Ingredents = pizza.Standard.Select(x => x.Description()).ToList(),
+						Price = pizza.StandardPrice,
+					}
+					);
+			}
+			if (pizzaMenuModel != null)
+			{
+				return new OkObjectResult(pizzaMenuModel);
+			}
+			else
+			{
+				return NoContent();
+			}
+		}
+	}
 }
---
+++ Lab3Pizzerian/Lab3Pizzerian/Models/PizzaMenuModel.cs
@@ -0,0 +1,15 @@
+﻿using Lab3Pizzerian.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Pizzerian.Models
+{
+	public class PizzaMenuModel
+	{
+		public string Name { get; set; }
+		public List<string> Ingredents { get; set; }
+		public int Price { get; set; }
+	}
+}
