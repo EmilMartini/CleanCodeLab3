b53f50023b124c4df38cf7ab546f0800369fefde
Daniel <daniel.andersson@iths.se>
2020-12-07 13:59:50 +0100

    add comments

--- Lab3Pizzerian/Lab3Pizzerian/Models/Order.cs
+++ Lab3Pizzerian/Lab3Pizzerian/Models/Order.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Lab3Pizzerian.Enumerations;
+using Lab3Pizzerian.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lab3Pizzerian
 {
-    public class Order
-    {
-        public Guid ID { get; set; }
-        public List<Pizza> Food { get; set; }
-        public Dictionary<Drink, int> Drinks { get; set; }
-    }
+	public class Order
+	{
+		public Guid ID { get; set; }
+		public List<Pizza> Pizzas { get; set; }
+		public List<EnumDrink> Drinks { get; set; }
+		public Dictionary<EnumDrink, int> DrinksX { get; set; } // ?? ska nog inte vara här
+		// kanske en klass som heter typ Priser? där vi har några Dictionarys med enums och priser
+	}
 }
